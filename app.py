d81fceef745ea12c3a4e8729014a1df45024c2c3
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-13 20:03:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,18 +2,13 @@ from flask import *
 
 app = Flask(__name__)
 app.debug = True
-app.secret_key = 'Programmer-RD-AI'
+app.secret_key = "Programmer-RD-AI"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('./home/home.html')
+    return render_template("./home/home.html")
 
-@app.route('/Lessons')
-def lesson():
-    return render_template('./home/lessons.html')
 
-@app.route('/Science A+')
-def science_a_plus():
-    return render_template('./home/science-A+.html')
-if __name__ == '__main__':
-    app.run(host='192.168.1.9')
+if __name__ == "__main__":
+    app.run()
